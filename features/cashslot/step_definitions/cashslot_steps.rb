436f799766b87de99f3e7d68a84e856e5870a52a
Zao2388 <betoseru23@gmail.com>
2016-06-27 20:19:59 -0600

    Making some code refactoring and better organized structure

--- features/cashslot/step_definitions/cashslot_steps.rb
+++ features/cashslot/step_definitions/cashslot_steps.rb
@@ -1,62 +1,72 @@
-class Account
-  def deposit(amount)
-    @balance = amount
-  end
+##==> Moved to lib/nice_bank.rb 
+
+# class Account
+#   def deposit(amount)
+#     @balance = amount
+#   end
   
-  def balance
-    @balance
-  end
-end
+#   def balance
+#     @balance
+#   end
+# end
 
-class CashSlot
-  def contents
-    @contents or raise("I'm empty!")
-  end
-  def dispense(amount)
-    @contents = amount
-  end
-end
+# class CashSlot
+#   def contents
+#     @contents or raise("I'm empty!")
+#   end
+#   def dispense(amount)
+#     @contents = amount
+#   end
+# end
 
-class Teller
-  def initialize(cash_slot)
-    @cash_slot = cash_slot
-  end
+# class Teller
+#   def initialize(cash_slot)
+#     @cash_slot = cash_slot
+#   end
 
-  def withdraw_from(account, amount)
-    @cash_slot.dispense(amount)
-  end
-end
+#   def withdraw_from(account, amount)
+#     @cash_slot.dispense(amount)
+#   end
+# end
 
-module KnowsTheDomain
-  def my_account
-    @my_account ||= Account.new
-  end
+##==> Moved to features/support/world_extensions.rb 
 
-  def cash_slot
-    @cash_slot ||= CashSlot.new
-  end
+# module KnowsTheDomain
+#   def my_account
+#     @my_account ||= Account.new
+#   end
 
-  def teller
-    @teller ||= Teller.new(cash_slot)
-  end
-end
+#   def cash_slot
+#     @cash_slot ||= CashSlot.new
+#   end
 
-World(KnowsTheDomain)
+#   def teller
+#     @teller ||= Teller.new(cash_slot)
+#   end
+# end
 
-CAPTURE_A_NUMBER = Transform /^\d+$/ do |number|
-  number.to_i
-end
+# World(KnowsTheDomain)
 
-Given /^I have deposited \$(#{CAPTURE_A_NUMBER}) in my account$/ do |amount|
-  my_account = Account.new
-  my_account.deposit(amount)
+##==> Moved to features/support/transforms.rb 
+
+
+# CAPTURE_A_NUMBER = Transform /^\d+$/ do |number|
+#   number.to_i
+# end
+
+Given /^my account has been credited with \$(#{CAPTURE_CASH_AMOUNT})$/ do |amount|
+  my_account.credit(amount)
   Wrong.assert{my_account.balance == amount}
 end
 
-When /^I withdraw \$(\d+)$/ do |amount|
+When /^I withdraw \$(#{CAPTURE_CASH_AMOUNT})$/ do |amount|
   teller.withdraw_from(my_account, amount)
 end
 
-Then /^\$(\d+) should be dispensed$/ do |amount|
+Then /^\$(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/ do |amount|
   Wrong.assert{@cash_slot.contents == amount}
+end
+
+Then /^the balance of my account should be \$(#{CAPTURE_CASH_AMOUNT})$/ do |amount|
+  Wrong.assert{my_account.balance == amount}
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,4 @@
 #require 'wrong'
+require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'nice_bank')
+
+puts "Here is what we load before running Scenarios"
